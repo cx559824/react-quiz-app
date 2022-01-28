635a42d9b76006d8ec699d52d743e943266c39fd
Rodolfo R. Raquion III <cb_r12@yahoo.com>
2022-01-28 17:58:53 +0800

    commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div style={{ textAlign: "center", marginBottom: 10 }}>
+      Made with love by{" "}
+      <a
+        href="https://www.facebook.com/ditter.raquion.9/"
+        style={{ cursor: "pointer" }}
+      >
+        Rodolfo Raquion
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
